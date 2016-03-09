ce6d105c217888ac41db7ce35696e2ad213c624d
Frag1k <p7quk@yandex.ru>
2016-03-09 10:38:13 +0300

    First commit

---
+++ app/models.py
@@ -0,0 +1,73 @@
+from app import db, login_manager
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.timestamp = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Post %r>' % self.body
+
+
+class Hostel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    adress = db.Column(db.String(140))
+    rooms = db.relationship('Room', backref='hostel', lazy='dynamic')
+
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    numbers_of_person = db.Column(db.Integer())
+    hostel_id = db.Column(db.Integer, db.ForeignKey('hostel.id'))
+    person = db.relationship('Person', backref='room', lazy='dynamic')
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    department = db.Column(db.String(50))
+    group = db.Column(db.Integer())
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
---
+++ app/views.py
@@ -0,0 +1,96 @@
+from flask import render_template, request, flash, redirect, url_for, g
+from flask_login import logout_user, login_user, current_user, login_required
+from app import app, db, models
+from sqlalchemy import desc
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        posts = models.Post.query.order_by(desc(models.Post.timestamp)).limit(100).all()
+        return render_template('index.html', posts=posts)
+    login()
+
+
+@app.route('/hostels', methods=['GET', 'POST'])
+@login_required
+def rooms():
+    hostels = models.Hostel.query.all()
+    return render_template('hostels.html', hostels=hostels)
+
+
+@app.route('/hostels/<hostel>')
+def hostel_detail(hostel):
+    rooms = models.Room.query.filter_by(hostel_id=hostel).all()
+    return render_template('hostel_view.html', rooms=rooms)
+
+
+@app.route('/rooms/<room>')
+def room_detail(room):
+    persons = models.Person.query.filter_by(room_id=room).all()
+    return render_template('room_view.html', persons=persons)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = models.User(request.form['username'], request.form['password'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    remember_me = False
+    if 'remember_me' in request.form:
+        remember_me = True
+    registered_user = models.User.query.filter_by(username=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user, remember=remember_me)
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = models.User.query.filter_by(username=username).first()
+    if user == None:
+        flash('User %s not found.' % username)
+        return redirect(url_for('index'))
+    return render_template('profile.html', user=user)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+@login_required
+def new():
+    if request.method == "GET":
+        return render_template('new.html')
+    else:
+        post = models.Post(request.form['title'], request.form['body'])
+        db.session.add(post)
+        db.session.commit()
+        flash('Todo item was successfully created')
+        return redirect(url_for('index'))
