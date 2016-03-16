e6ea004c91685516922ca6011862d7184f9d2904
frag1k <p7quk@yandex.ru>
2016-03-16 20:09:31 +0200

    edit models, update plots

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ SECRET_KEY = 'you-will-never-guess'
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
