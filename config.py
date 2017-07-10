8a3ba3d4020af288105440fd4932b5376df2f3a2
Your Name <you@example.com>
2017-07-10 19:41:53 +0300

    fix

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/hostel'
+SQLALCHEMY_DATABASE_URI = 'postgresql://hostel:hostelsp@localhost/hostel'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/hostel'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
